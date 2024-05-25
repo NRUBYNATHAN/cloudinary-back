3e8500b93b2c4e781a6dd899480c9a782e63854a
RUBYNATHAN N <rubynathan999@gmail.com>
2024-05-25 19:41:03 +0530

    cors-fix

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import bodyParser from "body-parser";
 import { connectDB } from "./config/db.js";
 import { ErrorHandler } from "./middlewares/error.js";
 import videoRoutes from "./routes/videos.js";
 import signUploadRoutes from "./routes/sign-upload.js";
-// import { videoRoutes } from "./routes/videos.js";
-// import { signUploadRoutes } from "./routes/sign-upload.js";
 
 dotenv.config();
 
@@ -15,26 +12,8 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const allowedOrigins = ["https://dulcet-dusk-812a69.netlify.app"];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-};
-
-app.use(cors(corsOptions));
 //MIddlewares
-// app.use(cors());
-app.use(bodyParser.json({ limit: "50mb" })); // Adjust the limit as needed
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 //routes
